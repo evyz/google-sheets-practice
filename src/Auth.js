688c82439b0787849fc35aa0fcb425353b359588
ABOBA123 <tarasovlev7@gmail.com>
2023-03-26 21:43:43 +0300

    asd

--- src/Auth.js
+++ src/Auth.js
@@ -1,8 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Login from "./components/login/Login";
 function Auth({ children }) {
   const [user, setUser] = useState(null);
+  useEffect(()=>{
+    let nickname = localStorage.getItem("nickname")
+    if (nickname){
+      setUser(nickname)
+    }
+  },[])
   if (!user || user === "") {
     return <Login user={user} setUser={setUser} />;
   }
