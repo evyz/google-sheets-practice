f0017ce68733dec2888e672ef19bd536b41ce757
evyz <endermainYT2502@gmail.com>
2023-04-22 22:34:35 +0300

    upd

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const ws = require("express-ws")(app);
 
 let { rooms, users, connections } = require("./utils");
 
@@ -17,6 +18,21 @@ app.use("/user", userApi);
 app.use("/room", roomApi);
 app.use("/connect", connectionApi);
 
+app.ws("/room/:id", (ws, req) => {
+  let { nickname } = req.query;
+  if (!nickname) {
+    ws.close();
+  }
+
+  ws.on("message", function (msg) {
+    try {
+      JSON.parse(msg);
+    } catch (err) {
+      ws.send("message must be JSON format");
+    }
+    ws.send(msg);
+  });
+});
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
 });
